60cf0229b9007d7ecbe978f9007d9f55f2a5b3af
Gustavo Parcianello Cardona <gupcardona@gmail.com>
2024-04-16 20:48:56 -0300

    Refactor code to create a tree with the user choice

--- binary_tree/src/Main.java
+++ binary_tree/src/Main.java
@@ -1,61 +1,54 @@
 import java.util.*;
 
-/** Gustavo Parcianello Cardona binary tree implementation. */
+/**
+ * Gustavo Parcianello Cardona binary tree implementation.
+ */
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main( String[] args ) {
         BinaryTree binaryTree = new BinaryTree();
         DotFile dotFile = new DotFile();
 
-        // original numbers: 10, 5, 20, 3, 7, 31, 12
-        List<Integer> originalNumbers = (Arrays.asList(10, 5, 20, 3, 7, 31, 12));
+        String generatedName = generateRandomName();
 
-        for (Integer number : originalNumbers) {
-            binaryTree.insert(number, binaryTree.root);
+        Scanner scanner = new Scanner( System.in );
+
+        while ( true ) {
+            System.out.println();
+            System.out.println( "Which operation do you want to perform?" );
+            System.out.println( "1 - Insert a new node" );
+            System.out.println( "2 - Remove a node" );
+            System.out.println( "else - Exit" );
+
+            int operation = scanner.nextInt();
+            int value;
+
+            if ( operation == 1 ) {
+                System.out.println( "Enter the value of the node you want to insert:" );
+                value = scanner.nextInt();
+                binaryTree.insert( value, binaryTree.root );
+            } else if ( operation == 2 ) {
+                System.out.println( "Enter the value of the node you want to remove:" );
+                value = scanner.nextInt();
+                binaryTree.remove( value, binaryTree.root );
+            } else {
+                System.out.println( "Exiting..." );
+                break;
+            }
         }
 
-        /** TODO: LET ONLY THE TREE THAT YOU WANT TO GENERATE WITHOUT COMMENT
-         *  TODO: AND COMMENT THE OTHERS.
-         *
-         *  TODO: EXAMPLE: IF YOU WANT TO GENERATE THE ORIGINAL BINARY TREE,
-         *  TODO: LET ONLY THE FIRST DOTFILE.GENERATEDOTFILE(...) AND THE PRINTINFO(...),
-         *  TODO: AND COMMENT THE OTHERS.
-         */
+        dotFile.generateDotFile( String.format( "%s.dot", generatedName ), binaryTree );
+        printInfo( binaryTree, generatedName );
+        System.out.println();
+        System.out.println( "The dot file was generated.");
+        System.out.println( "You can use the following command to generate the image:" );
+        System.out.println( "dot -Tsvg generated/dot-files/" + generatedName + ".dot -o generated/svg-files/" + generatedName + ".svg" );
+    }
 
-        /** original binary tree means that there was no new insertions or deletions after the tree was created */
-//        dotFile.generateDotFile("original-binary-tree.dot", binaryTree);
-//        printInfo(binaryTree, "Original Binary Tree");
-
-        /** binary tree with five new insertion */
-//        binaryTree.insert( 1, binaryTree.root);
-//        binaryTree.insert(50, binaryTree.root);
-//        binaryTree.insert(32, binaryTree.root);
-//        binaryTree.insert(75, binaryTree.root);
-//        binaryTree.insert(4, binaryTree.root);
-//        dotFile.generateDotFile("binary-tree-with-five-new-insertions.dot", binaryTree);
-//        printInfo(binaryTree, "Binary Tree with Five New Insertions");
-
-        /** binary tree with one deletion */
-//        binaryTree.remove(20, binaryTree.root);
-//        dotFile.generateDotFile("binary-tree-with-one-deletion.svg", binaryTree);
-//        printInfo(binaryTree, "Binary Tree with One Deletion");
-
-        /** binary tree with two new insertions and one deletion */
-//        binaryTree.insert(50, binaryTree.root);
-//        binaryTree.insert(45, binaryTree.root);
-//        binaryTree.remove(20, binaryTree.root);
-//        dotFile.generateDotFile("binary-tree-with-two-new-insertions-and-one-deletion.dot", binaryTree);
-//        printInfo(binaryTree, "Binary Tree with Two New Insertions and One Deletion");
-
-        /** binary tree with five new insertion and one deletion */
-        binaryTree.insert( 1, binaryTree.root);
-        binaryTree.insert(50, binaryTree.root);
-        binaryTree.insert(32, binaryTree.root);
-        binaryTree.insert(75, binaryTree.root);
-        binaryTree.insert(4, binaryTree.root);
-        binaryTree.remove(5, binaryTree.root);
-        dotFile.generateDotFile("binary-tree-with-five-new-insertions-and-one-deletion.dot", binaryTree);
-        printInfo(binaryTree, "Binary Tree with Five New Insertions And One Deletion");
+    private static String generateRandomName() {
+        Random random = new Random();
+        String name = "binary-tree-";
+        return name + random.nextInt( 10000 );
     }
 
     private static void printInfo( BinaryTree binaryTree, String treeName ) {
@@ -63,38 +56,38 @@ public class Main {
         List<Integer> inOrderList = new ArrayList<>();
         List<Integer> postOrderList = new ArrayList<>();
 
-        binaryTree.preOrder(binaryTree.root, preOrderList);
-        binaryTree.inOrder(binaryTree.root, inOrderList);
-        binaryTree.postOrder(binaryTree.root, postOrderList);
+        binaryTree.preOrder( binaryTree.root, preOrderList );
+        binaryTree.inOrder( binaryTree.root, inOrderList );
+        binaryTree.postOrder( binaryTree.root, postOrderList );
 
-        int minValue = binaryTree.searchMin(binaryTree.root);
-        int maxValue = binaryTree.searchMax(binaryTree.root);
-
-        System.out.println();
-
-        System.out.println("+-----------------------------------------------------+");
-
-        System.out.println(treeName);
+        int minValue = binaryTree.searchMin( binaryTree.root );
+        int maxValue = binaryTree.searchMax( binaryTree.root );
 
         System.out.println();
 
-        System.out.print("minimum value: ");
-        System.out.println(minValue);
+        System.out.println( "+-----------------------------------------------------+" );
 
-        System.out.print("maximum value: ");
-        System.out.println(maxValue);
+        System.out.println( treeName );
 
         System.out.println();
 
-        System.out.print("pre order list:  ");
-        System.out.println(preOrderList);
+        System.out.print( "minimum value: " );
+        System.out.println( minValue );
 
-        System.out.print("in order list:   ");
-        System.out.println(inOrderList);
+        System.out.print( "maximum value: " );
+        System.out.println( maxValue );
 
-        System.out.print("post order list: ");
-        System.out.println(postOrderList);
+        System.out.println();
 
-        System.out.println("+-----------------------------------------------------+");
+        System.out.print( "pre order list:  " );
+        System.out.println( preOrderList );
+
+        System.out.print( "in order list:   " );
+        System.out.println( inOrderList );
+
+        System.out.print( "post order list: " );
+        System.out.println( postOrderList );
+
+        System.out.println( "+-----------------------------------------------------+" );
     }
 }
